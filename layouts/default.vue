958330450789af7a51377ce85dddfa7831789edb
Javier Buitrago <jbuitrago2014@fau.edu>
2018-04-29 16:11:38 -0400

    added first two posts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
+  <TheHeader />
   <main class="main-content">
     <nuxt/>
   </main>
   </div>
 </template>
+
+
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+
+  }
+}
+</script>
 
 <style>
 * {
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,41 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        javi-blog
-      </h1>
-      <h2 class="subtitle">
-        personal blog
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <nuxt-link
+    v-for="post in post"
+    :key="post.id"
+    :to="post.id">
+    <article class="post-preview"></article>
+    </nuxt-link>
+
+
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      posts: [
+      {
+          title: "Thoughts On The State of MMA",
+          previewText: "Current state of MMA",
+          thumbnailURL: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
+          id: "state-of-MMA"
+        },
+        {
+            title: "Goat talk",
+            previewText: "talking about the current goat of MMA",
+            thumbnailURL: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
+            id: "goat-of-MMA"
+          }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+
 </style>
