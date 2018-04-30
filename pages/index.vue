c06bca281b3440b7ff07c4ba945179ed12d17a36
Javier Buitrago <jbuitrago2014@fau.edu>
2018-04-29 20:18:29 -0400

    added first two posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <section class="container">
     <nuxt-link
-    v-for="post in post"
+    v-for="post in posts"
     :key="post.id"
     :to="post.id">
-    <article class="post-preview"></article>
+    <article class="post-preview">
+      <div
+        :style="{backgroundImage: post.thumbnailUrl}"
+        class="post-preview-thumbnail"></div>
+      <div class="post-preview-content">
+        <h1>{{ post.title}}</h1>
+        <p>{{post.previewText}}</p>
+      </div>
+    </article>
     </nuxt-link>
 
 
@@ -21,13 +29,13 @@ export default {
       {
           title: "Thoughts On The State of MMA",
           previewText: "Current state of MMA",
-          thumbnailURL: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
+          thumbnailUrl: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
           id: "state-of-MMA"
         },
         {
             title: "Goat talk",
             previewText: "talking about the current goat of MMA",
-            thumbnailURL: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
+            thumbnailUrl: "https://cdn.images.express.co.uk/img/dynamic/167/590x/Conor-McGregor-Khabib-Nurmagomedov-908222.jpg",
             id: "goat-of-MMA"
           }
       ]
