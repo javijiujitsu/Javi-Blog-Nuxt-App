889da9cacd4673bf3fb11282dd89272eb7717e73
Javier Buitrago <jbuitrago2014@fau.edu>
2018-05-10 23:00:20 -0400

    rendering about page dynamically with Vue

---
+++ pages/_postId/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="post">
+    <div class="post-thumbnail"></div>
+    <h1></h1>
+    <p></p>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+    asyncData(context){
+      return context.app.$storyapi
+      .get("cdn/stories/blog/" + context.params.postId, {
+        version: "draft"
+      })
+      .then(res =>  {
+          console.log(res.data);
+
+      });
+    }
+};
+
+
+
+</script>
