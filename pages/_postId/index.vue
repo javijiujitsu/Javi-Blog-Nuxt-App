6a2f571c52f5d2ee848335dd735627911f9756fb
Javier Buitrago <jbuitrago2014@fau.edu>
2018-05-13 17:00:07 -0400

    added editing abilities the post and about page inside Storyblok CMS

--- pages/_postId/index.vue
+++ pages/_postId/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="post">
+  <div id="post" v-editable="blok">
     <div class="post-thumbnail" :style="{backgroundImage: 'URL(' + image +')'}"></div>
     <section class="post-content">
     <h1> {{ title }}</h1>
@@ -20,10 +20,17 @@ export default {
       })
       .then(res =>  {
         return {
+          blok: res.data.story.content,
           image: res.data.story.content.thumbnail,
           title: res.data.story.content.title,
           content: res.data.story.content.content
         };
+      });
+    },
+    mounted() {
+      this.$storyblok.init()
+      this.$storyblok.on("change", () => {
+          location.reload(true);
       });
     }
 };
